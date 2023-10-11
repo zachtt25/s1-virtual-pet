c006d888de758ef32f006326d82dd49121f94288
Zach Tan <zachtt25@ausdk12.org>
2023-10-11 11:37:56 -0700

    commit

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,7 +1,33 @@
+import javax.swing.*;
 public class Runner{
-    public static void main(String[] args){
+    public Runner(){
         VirtualPet v = new VirtualPet();
+        v.exercise();
+        takeABeat(1000);
         v.feed();
-        v.sleep();
+        String ans = getAnswer("How are you?");
+        if(ans.equalsIgnoreCase("Good")){
+
+        }
     }
+
+    public void takeABeat(int milliseconds){
+        try {
+            Thread.sleep(milliseconds); //milliseconds
+        } catch(Exception e){
+
+        }
+    }
+
+    String s = (String)JOptionPane.showInputDialog(
+                    new JFrame(),
+                    "Your Message",
+                    "Your Title",
+                    JOptionPane.PLAIN_MESSAGE
+    );
+
+    public static void main(String[] args){
+        new Runner();
+    }
+
 }
